eb8ee639e8d0019b4bcbac3c2c1e69f544b93021
dingfen <df12138@mail.ustc.edu.cn>
2019-03-10 23:09:04 +0800

    add -c -w args parse.

--- CommandLineParse.cpp
+++ CommandLineParse.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <string>
 #include <ctype.h>
 using namespace std;
 
+#define MAXFILENAME 64
 int main(int argc, char *argv[])
 {
     int opt;
-
+    // 文件名 要求的开头字母、结尾字母
+    string filename;
     char head = '\0';
     char tail = '\0';
     // 要求单词链有几个单词
@@ -15,35 +18,34 @@ int main(int argc, char *argv[])
     // 统计字母时为 false 统计单词为 true
     bool wordOrLetter = true;
 
-    const char *optstring = "cwh:t:n:";
+    const char *optstring = "c:w:h:t:n:";
     while ((opt = getopt(argc, argv, optstring)) != -1)
     {
         switch (opt)
         {
         case 'c':
             wordOrLetter = false;
+            filename = optarg;
             break;
         case 'w':
             wordOrLetter = true;
+            filename = optarg;
             break;
         case 'h':
             if (isalpha(*optarg))
                 head = *optarg;
             else
                 cerr << "-h must be followed by a alpha" << endl;
-            // cout<<head<<endl;
             break;
         case 't':
             if (isalpha(*optarg))
                 tail = *optarg;
             else
                 cerr << "-t must be followed by a alpha" << endl;
-            // cout<<tail<<endl;
             break;
         case 'n':
             if (isdigit(*optarg))
                 numWord = *optarg - '0';
-            // cout<<numWord<<endl;
             else
                 cerr << "-n must be followed by a interger" << endl;
             break;
@@ -56,12 +58,10 @@ int main(int argc, char *argv[])
     // cout << optind <<endl;
 
     ifstream fin;
-    char *q = new char[5];
-    fin.open(argv[optind], ios::in);
-    fin >> q;
-    cout << q << endl;
-
-    delete[] q;
+    fin.open(filename.c_str(), ios::in);
+    if(!fin) {
+        cerr<<"The file "<<filename.c_str()<<"cannot be found.\n"<<endl;
+    }
     fin.close();
     return 0;
 }
