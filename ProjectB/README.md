0637d71a3d7fa6a4b2caef04afb88f6ad96de3ed
jiachenglian <603688712@qq.com>
2019-03-30 20:03:05 +0800

    finish doc v1.0

--- ProjectB/README.md
+++ ProjectB/README.md
@@ -16,22 +16,23 @@ C++11
 ## Core类 API接口
 
 ```C++
-    /* vector<string> gen_chain_word(vector<string> words, char head, char tail);
+    /* vector<string> Core::gen_chain_word(vector<string> words, char head, char tail);
     * 这个函数接受三个参数, words 为输入的单词列表, head 为所要求的单词链的头字母, 
     * tail 万所要求的单词链尾字母,函数返回值为所要求的最长单词个数的单词链 vector<string>
     */
-    vector<string> gen_chain_word(vector<string> words, char head, char tail);
+    vector<string> Core::gen_chain_word(vector<string> words, char head, char tail);
 
-    /* vector<string> gen_chain_char(vector<string> words, char head, char tail);
+    /* vector<string> Core::gen_chain_char(vector<string> words, char head, char tail);
     * 这个函数接受三个参数, words 为输入的单词列表, head 为所要求的单词链的头字母, 
     * tail 万所要求的单词链尾字母,函数返回值为所要求的最多字母个数的单词链 vector<string>
     */
-    vector<string> gen_chain_char(vector<string> words, char head, char tail);
-    /* vector<vector<string>> gen_chain_word(vector<string> words, int num, char head, char tail);
+    vector<string> Core::gen_chain_char(vector<string> words, char head, char tail);
+
+    /* vector<vector<string>> Core::gen_chain_word(vector<string> words, int num, char head, char tail);
     * 这个函数接受三个参数, words 为输入的单词列表, head 为所要求的单词链的头字母, 
     * tail 万所要求的单词链尾字母,函数返回值为所要求的单词链 num 为所要求的字母链的个数
     */
-    vector<vector<string>> gen_chain_word(vector<string> words, int num, char head, char tail);
+    vector<vector<string>> Core::gen_chain_word(vector<string> words, int num, char head, char tail);
 
     /* 若对首尾字母不做要求，则置head, tail 为 \0, 若对单词链长度不做要求，则置num为-1
      */
@@ -43,4 +44,4 @@ C++11
 
 `> make`
 
-Usage:  
+Usage:  执行`make`后会在当前目录下生成`result`文件夹，打开文件夹中`index.html`可以看到各文件、函数的测试覆盖率
--- ProjectB/src/test.cpp
+++ ProjectB/src/test.cpp
@@ -7,29 +7,61 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    vector<vector<string>> wordsVec;
     string input[] = {"qwe", "asd", "re", "dwe", "efs", "sfef", "cqualqwdweqq", "sad", "sdds"};
-    vector<string> words(begin(input), end(input));
-    vector<string> noword;
-
-    vector<string> retWord[5];
-    retWord[0] = Core::gen_chain_word(words, '\0', '\0');
-    retWord[1] = Core::gen_chain_word(words, 's', '\0');
-    retWord[2] = Core::gen_chain_word(words, '\0', 's');
-    retWord[3] = Core::gen_chain_word(words, 's', 's');
-    retWord[4] = Core::gen_chain_word(noword, '\0', '\0');
-
-    vector<string> retChar[4];
-    retChar[0] = Core::gen_chain_char(words, '\0', '\0');
-    retChar[1] = Core::gen_chain_char(words, 's', '\0');
-    retChar[2] = Core::gen_chain_char(words, '\0', 's');
-    retChar[3] = Core::gen_chain_char(words, 's', 's');
-
-    vector<vector<string>> retList[4];
-    retList[0] = Core::gen_chain_word(words, 3, '\0', '\0');
-    retList[1] = Core::gen_chain_word(words, 3, 's', '\0');
-    retList[2] = Core::gen_chain_word(words, 3, '\0', 's');
-    retList[3] = Core::gen_chain_word(words, 3, 's', 's');
+    wordsVec.push_back(vector<string>(end(input)-1, end(input)));
+    for (int i=0; i<4; i++)
+        wordsVec.push_back(vector<string>(begin(input), end(input)));
     
+    char head[] = {'\0', '\0', 's', '\0', 's'};
+    char tail[] = {'\0', '\0', '\0', 's', 's'};
+    
+    //test for vector<string> Core::gen_chain_word(vector<string> words, char head, char tail);
+    vector<string> retWord[5];
+    
+    cout << "test for API 1" << endl;
+    for (int i=0; i<5; i++)
+    {
+        cout << "case " << i << ": ";
+        retWord[i] = Core::gen_chain_word(wordsVec[i], head[i], tail[i]);
+        for (auto str : retWord[i])
+            cout << str << " ";
+        cout << endl;
+    }
+    cout << endl;
+    
+    //test for vector<string> Core::gen_chain_char(vector<string> words, char head, char tail);
+    vector<string> retChar[5];
+    
+    cout << "test for API 2" << endl;
+    for (int i=0; i<5; i++)
+    {
+        cout << "case " << i << ": ";
+        retChar[i] = Core::gen_chain_char(wordsVec[i], head[i], tail[i]);
+        for (auto str : retChar[i])
+            cout << str << " ";
+        cout << endl;
+    }
+    cout << endl;
+    
+    //test for vector<vector<string>> Core::gen_chain_word(vector<string> words, int num, char head, char tail);
+    vector<vector<string>> retList[5];
+    
+    cout << "test for API 3" << endl;
+    for (int i=0; i<5; i++)
+    {
+        cout << "case " << i << ": ";
+        retList[i] = Core::gen_chain_word(wordsVec[i], 3, head[i], tail[i]);
+        cout << "number of word chains " << ":" << retList[i].size() << endl;
+        
+        for (auto vec : retList[i])
+        {
+            for (auto str : vec)
+                cout << str << " ";
+            cout << endl;
+        }
+        cout << endl;
+    }
     
     return 0;
 }
