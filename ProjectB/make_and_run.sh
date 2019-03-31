0f6ade25c80febb153b311adfb4dcd361e8dd427
dingfen <df12138@mail.ustc.edu.cn>
2019-03-31 13:53:02 +0800

    Modify the Makefile.

---
+++ ProjectB/make_and_run.sh
@@ -0,0 +1,12 @@
+make
+
+if ($(LCOV), ) then 
+	echo "No lcov found.Please install lcov first."
+fi
+
+FILE="test/test_5.txt test/test_6.txt test/test_4.txt test/test_9.txt test/test_11.txt"
+
+bin/WordList $FILE
+lcov -c -d . -o test.info
+genhtml -o result test.info
+rm  *.gcda *.gcno *.info
